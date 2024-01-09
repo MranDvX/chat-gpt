3cbcd2ff990caff6e068c64fa2a140299d2a6d93
Francisco Mancuello <franmavazq@gmail.com>
2024-01-09 16:19:18 -0300

    refactor: Dashboard text

--- src/presentation/layouts/DashboardLayout.tsx
+++ src/presentation/layouts/DashboardLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 export const DashboardLayout = () => {
     return (
         <>
-            <h1>Dahboard</h1>
+            <h1>Dashboard</h1>
             <Outlet />
         </>
     )
